6d04a48361331d68b76fc400c01244c5b99d4549
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-11-25 13:57:04 +0300

    update_delete

--- js/ui.js
+++ js/ui.js
@@ -89,10 +89,8 @@ class UI {
     deleteIconLink.id = film.id;
     deleteIconLink.addEventListener("click", deleteFilm);
 
-    const deleteIcon = document.createElement("div");
-    deleteIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg text-light" viewBox="0 0 16 16">
-    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-  </svg>`;
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa fa-remove";
 
     const editIconLink = document.createElement("a");
     editIconLink.href = "#";
